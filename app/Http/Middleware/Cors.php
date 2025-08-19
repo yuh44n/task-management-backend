57893a9b47bea9b11f7abfd69dff0b7ef1e12582
yuh44n <yassine.ezzahraoui02@gmail.com>
2025-08-19 21:20:34 +0100

    a new commit

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -30,7 +30,7 @@ class Cors
         } else {
             $response->headers->set('Access-Control-Allow-Origin', '*');
         }
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-CSRF-TOKEN, Accept');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
         $response->headers->set('Access-Control-Max-Age', '7200');
--- app/Http/Middleware/HandleOptions.php
+++ app/Http/Middleware/HandleOptions.php
@@ -30,7 +30,7 @@ class HandleOptions
                 $response->header('Access-Control-Allow-Origin', '*');
             }
             
-            $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
             $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-CSRF-TOKEN, Accept');
             $response->header('Access-Control-Allow-Credentials', 'true');
             $response->header('Access-Control-Max-Age', '7200');
